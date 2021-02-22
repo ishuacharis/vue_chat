cbda8b089c92fde89dd72328fe1e98dedd705e56
ishuacharis <ishuacharis@gmail.com>
2021-02-22 02:08:59 +0100

    add feeds and chats

---
+++ src/data/chats.js
@@ -0,0 +1,14 @@
+export const CHATS = [
+
+    {
+        name: "+2348030742614",
+        date: 'yesterday',
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. autem suscipit dolorum Lorem ipsum dolor sit amet consectetur adipisicing elit. autem suscipit dolorum"
+    },
+    {
+        name: "Sola Ishua",
+        date: '20/01/21',
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. autem suscipit dolorum Lorem ipsum dolor sit amet consectetur adipisicing elit. autem suscipit dolorum"
+    }
+
+];
---
+++ src/data/feeds.js
@@ -0,0 +1,17 @@
+export const FEEDS = [
+    {
+         text: "Lorem ipsum dolor sit amet consectetur, adipisicing ept. Ipsa, obcaecati nesciunt tempore amet corporis error illum hic corrupti placeat debitis expedita odit praesentium, ducimus recusandae voluptatem perspiciatis culpa? Dolorum, molestiae!"
+    },
+    {
+         text: "Lorem ipsum dolor sit amet consectetur, adipisicing ept. Ipsa, obcaecati nesciunt tempore amet corporis error illum hic corrupti placeat debitis expedita odit praesentium, ducimus recusandae voluptatem perspiciatis culpa? Dolorum, molestiae!"
+    },
+    {
+         text: "Lorem ipsum dolor sit amet consectetur, adipisicing ept. Ipsa, obcaecati nesciunt tempore amet corporis error illum hic corrupti placeat debitis expedita odit praesentium, ducimus recusandae voluptatem perspiciatis culpa? Dolorum, molestiae!"
+    },
+    {
+         text: "Lorem ipsum dolor sit amet consectetur, adipisicing ept. Ipsa, obcaecati nesciunt tempore amet corporis error illum hic corrupti placeat debitis expedita odit praesentium, ducimus recusandae voluptatem perspiciatis culpa? Dolorum, molestiae!"
+    },
+    {
+         text: "Lorem ipsum dolor sit amet consectetur, adipisicing ept. Ipsa, obcaecati nesciunt tempore amet corporis error illum hic corrupti placeat debitis expedita odit praesentium, ducimus recusandae voluptatem perspiciatis culpa? Dolorum, molestiae!"
+    }
+];

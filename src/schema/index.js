48d34f1f490440aa999dde6870c2b2913b67b5c5
ishuacharis <ishuacharis@gmail.com>
2021-03-12 02:46:17 +0100

    add validation schema

---
+++ src/schema/index.js
@@ -0,0 +1,8 @@
+import * as yup from "yup";
+
+
+export const validationSchema = yup.object().shape({
+    phoneNo: yup.string().required().label('Phone no'),
+    displayName: yup.string().required().label('Display name'),
+    about: yup.string().required().label('About me')
+});

66deedad028604c51aaff7cb2094e5fc6127462d
ishuacharis <ishuacharis@gmail.com>
2021-03-15 17:02:21 +0100

    add addUser routes

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -7,3 +7,7 @@ export const getToken = () => {
     
     return JSON.parse(localStorage.getItem("VUE_CHAT_USER"));
 }
+
+export const getUser = () => {
+    return JSON.parse(localStorage.getItem("VUE_CHAT_USER"))._id;
+}

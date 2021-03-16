84ca3f0649e79b7e9f20ae7319ad91ab1d0554cf
ishuacharis <ishuacharis@gmail.com>
2021-03-16 15:21:12 +0100

    add getFriends routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -38,3 +38,29 @@ export const register = async (args) => {
 export const addUser = async (args) => {
     return await connect(args);
 }
+
+export const getFriends = async(args) => {
+
+
+    const endPoint  = args["endPoint"];
+    const body =  args["body"];
+    const method = args["method"];
+    const token  = args['token'] || '';
+    const authorization = token ?  `Bearer ${token}` : '' ;
+    headers["headers"]["Authorization"] = authorization;
+    const uri = BASE_URI+endPoint; 
+    const options = {
+        method: method,
+        headers: headers["headers"],
+        body: JSON.stringify(body)
+    }
+    const response = await fetch(uri,options)
+    if ( !response.ok ) {
+        const error = await response.json();
+        
+        throw new Error(error["message"]);
+    }
+
+    return await response.json();
+
+}
